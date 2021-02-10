0aa70aa4776172ab16d9cd0b3f08089da08c2043
Candiepih <mutheeal.am@gmail.com>
2021-02-10 16:20:29 +0300

    handled static route paths

--- config/settings.py
+++ config/settings.py
@@ -133,4 +133,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/html", ".css", True)
