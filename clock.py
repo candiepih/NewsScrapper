d5a594873f337340744408dcad77dfe25b387aa9
Candiepih <mutheeal.am@gmail.com>
2021-03-04 21:20:38 +0300

    changed scheduling time interval

--- clock.py
+++ clock.py
@@ -24,7 +24,7 @@ def crawl():
         d.addBoth(lambda _: reactor.stop())
 
 
-@sched.scheduled_job('interval', minutes=50)
+@sched.scheduled_job('interval', minutes=25)
 def timed_job():
     crawl()
 
