8d0a8e9712a87d6b7b4aba5787ff009d4a850376
Candiepih <mutheeal.am@gmail.com>
2022-09-10 12:27:54 +0300

    Changed environment env

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (env('IS_PRODUCTION').lower() == 'true')
+DEBUG = (env('IS_DEVELOPMENT').lower() == 'true')
 
 ALLOWED_HOSTS = ['newscrapperapi.herokuapp.com', '127.0.0.1', 'localhost']
 CORS_ORIGIN_ALLOW_ALL = True
