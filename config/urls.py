54fa3c41c43ca1e0596c5e2271cf46381ba4195e
Candiepih <mutheeal.am@gmail.com>
2022-09-10 12:37:34 +0300

    Modified routes cache time to 30 minutes

--- config/urls.py
+++ config/urls.py
@@ -19,11 +19,14 @@ from django.urls import path
 from api import views
 from django.views.decorators.cache import cache_page
 
-# Cache time to live is 15 minutes for each view.
+# Cache time to be 30 minutes
+cache_time = (60 * 30)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/news/categories', cache_page(1)(views.News.as_view()), name="AllNews"),
-    path('api/news/categories/<str:category>', cache_page(1)(views.NewsCategory.as_view()), name="NewsCategory"),
+    path('/', cache_page(cache_time)(views.News.as_view()), name="AllNews"),
+    path('categories/<str:category>', cache_page(cache_time)(views.NewsCategory.as_view()),
+         name="NewsCategory"),
 ]
 
 handler404 = views.Error404.as_view()
